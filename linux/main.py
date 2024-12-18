92ee4553f5a481ed1f6aa919553d43aaffb60037
4bitFox <33175205+4bitFox@users.noreply.github.com>
2024-12-18 10:28:03 +0100

    Update main.py

--- linux/main.py
+++ linux/main.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+
+import MiniMixer as MM
+
+MM.serial.init("3273e2cefa50a16eafefca053ba87625")
+while True:
+    serial = MM.serial.serial_data
+    if serial != None:
+        #print(serial)
+        serial_dict = MM.evaluate.serial2dict(serial)
+        print(serial_dict)
